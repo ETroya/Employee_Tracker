1ef36d12dd4e4771d2bda6fea18138424c95bfdd
Edwin Troya <edtroyajr@gmail.com>
2021-02-08 23:14:53 -0800

    working table

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const inquirer =require ("inquirer")
 const mysql = require ("mysql")
+const cTable = require('console.table');
+
 const connection =mysql.createConnection({
     hot:' localhost',
     port:3306,
@@ -25,12 +27,68 @@ connection.connect((err)=>{
         type:"list",
         message: "What would you like to do?",
         choice:[
-          "View All Employers?",
-          "View All Employee's by Department",
-          "Update Employee information",
           "Add Employee",
-          "Add Role",
-          "Add Department"
+          "Remove Employee",
+          "View All Employees",
+          "Update Employee Role",
+          "Add Department",
+          "View All Departments",
+          "Add Roles",
+          "View All Roles",
+          "Exit"
         ]
-      }])
-  }
+      }
+      ])
+
+    .then(function(res){
+    switch (res.start){
+      case "Add Employee":
+        addEmployee();
+        break;
+
+        case "Remove Employee":
+          removeEmployee();
+        break;
+        
+        case "View All Employees":
+          viewAllEmployees();
+        break;
+
+        case "Update Employee Role":
+          updateEmployeeRole();
+        break;
+
+        case "Add Department":
+          addDepartment();
+        break;
+        
+        case "View All Departments":
+          viewAllDepartment();
+        break;
+
+        case "Add Roles":
+        addRole();
+        break;
+
+        case "View All Roles":
+        viewAllRoles();
+        break;
+
+        case "Exit":
+        connection.end();
+        break;
+    }
+  })  
+}
+
+function viewAllEmployees(){
+  connection.query("SELECT employee.first_name, employee.last_name,, role.title, role.salary, department.name, CONCAT(e.first_name, '' ,e.last_name)As Manager From employee Inner JOIN role.id = employee.role_id INNER JOIN department on department.id = left join employee e on employee.manager_id =e.id,",
+  function (err, res) {
+    if (err, res){
+      if (err) throw err
+      console.table(res)
+      start()
+    }
+  })
+}
+
